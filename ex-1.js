202a39995a5432409998e36b46d03bb5c8db21d2
Namfon <varisa.vb@hotmail.com>
2024-07-10 14:20:03 +0700

    feat: add algo ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,17 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      let sum = numbers[i] + numbers[j];
+      if (sum === target) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+}
+
+numbersTest = [2, 7, 11, 15];
+
+console.log(twoSum(numbersTest, 9));
+console.log(twoSum(numbersTest, 23));
+console.log(twoSum(numbersTest, 18));
